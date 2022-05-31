f4ed4b64d27a099e77519fe21f205919c67bf9e8
xiaoyo123 <36474975azl@gmail.com>
2022-05-31 19:49:08 +0800

    week3 modify

--- c++1102intern/week3/main.cpp
+++ c++1102intern/week3/main.cpp
@@ -2,7 +2,7 @@
 #include "solve.h"
 #include "stdio.h"
 
-void filldeck(card *wdeck, const char * wface[], const char * wsuit[]) {
+void filldeck(card* wdeck, const char* wface[], const char* wsuit[]) {
 	for (int i = 0; i <= 51; i++) {
 		wdeck[i].faceid = i % 13;
 		wdeck[i].suitid = i / 13;
@@ -14,13 +14,13 @@ void filldeck(card *wdeck, const char * wface[], const char * wsuit[]) {
 int main() {
 	card deck[52];
 
-	const char *face[] = { "Ace", "Deuce", "Three",
+	const char* face[] = { "Ace", "Deuce", "Three",
 							"Four", "Five",
 							"Six", "Seven", "Eight",
 							"Nine", "Ten",
 							"Jack", "Queen", "King" };
 
-	const char *suit[] = { "Spades", "Hearts", "Diamonds", "Clubs"};
+	const char* suit[] = { "Spades", "Hearts", "Diamonds", "Clubs" };
 
 	filldeck(deck, face, suit);
 	output(deck);
@@ -39,12 +39,12 @@ int main() {
 	output(deck);
 	printf("\n");
 
-	for(int i = 0; i < 52; i++)
+	for (int i = 0; i < 52; i += 13)
 		bubble(deck, i + 13, sort1);
 	output(deck);
 	printf("\n");
 
-	for(int i = 0; i < 52; i += 13)
+	for (int i = 0; i < 52; i += 13)
 		bubble(deck, i + 13, sort2);
 	output(deck);
 	printf("\n");
